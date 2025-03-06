3e83882979cbb3a17121fb18c8182d5a0e9414c7
Miguel-Leao <miguelsavioleao@gmail.com>
2025-03-06 12:02:44 -0400

    criando o load posts e alterando css

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,40 @@ import "./App.css";
 //
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'O corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'O corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'O corpo 3'
-      },
-    ]
+    posts: []
   };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    const [ posts ] = await Promise.all([postsResponse]);
+
+    const postsJson = await posts.json();
+
+    this.setState({ posts: postsJson});
+  }
 
   render() {
     const { posts } = this.state;
 
+
     return (
-      <div className="App">
+      <section className="container">
+         <div className="posts">
         {posts.map(post => (
-          <div key={post.id}>
+          <div key={post.id} className="post">
+          <div className="post-content">
             <h1>{post.title}</h1>
             <p>{post.body}</p>
           </div>
+          </div>
         ))}
       </div>
+      </section>
     );
   }
 }
