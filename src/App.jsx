3467fc02cfbf457c2bbafc4ed73e57be23baf6cc
Miguel-Leao <miguelsavioleao@gmail.com>
2025-03-12 12:02:25 -0400

    finalização do photos e inicio da componentização

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./App.css";
+import { PostCard } from "./components/PostCard";
 
 
 //
@@ -37,13 +38,7 @@ class App extends Component {
       <section className="container">
          <div className="posts">
           {posts.map(post => (
-            <div key={post.id} className="post">
-              <img src={post.cover} alt={post.title}/>
-              <div className="post-content">
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-               </div>
-             </div>
+            <PostCard key={post.id} post={post}/>
             ))}
           </div>
         </section>
